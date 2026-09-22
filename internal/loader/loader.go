b19ab753ab1ef24fad7b2a8014f6f33a7a427a2d
Eugene Palchukovsky <dev@palchukovsky.com>
2026-09-22 09:53:38 +0300

    Release 0.8.2

--- internal/loader/loader.go
+++ internal/loader/loader.go
@@ -35,7 +35,7 @@ import (
 
 // SDKVersion is the version baked into the Go SDK source tree. It is used both
 // for runtime cache namespacing and for the runtime/SDK compatibility check.
-const SDKVersion = "0.8.1"
+const SDKVersion = "0.8.2"
 
 const (
 	envRuntimePath  = "OPENPIT_RUNTIME_LIBRARY_PATH"
